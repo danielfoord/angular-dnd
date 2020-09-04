411b804193661f22d2b5538bbd44c0f29c5b073c
Daniel Foord <danielfoord@users.noreply.github.com>
2020-09-04 22:26:28 +0200

    Scaffolding

---
+++ projects/angular-dnd/src/lib/angular-dnd.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AngularDndComponent } from './angular-dnd.component';
+
+describe('AngularDndComponent', () => {
+  let component: AngularDndComponent;
+  let fixture: ComponentFixture<AngularDndComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AngularDndComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AngularDndComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/angular-dnd/src/lib/angular-dnd.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-dnd',
+  template: `
+    <p>
+      angular-dnd works!! :)
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AngularDndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
