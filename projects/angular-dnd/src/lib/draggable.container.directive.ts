4a1be3f5834e78adb75df2bc5fae70ad7a69070b
Daniel Foord <danielfoord@users.noreply.github.com>
2020-09-05 17:25:42 +0200

    element positioning

--- projects/angular-dnd/src/lib/draggable.container.directive.ts
+++ projects/angular-dnd/src/lib/draggable.container.directive.ts
@@ -5,13 +5,32 @@ import { Directive, HostListener, ElementRef } from '@angular/core';
 })
 export class DraggableContainerDirective {
 
+  constructor(private elementRef: ElementRef) { }
+
   @HostListener('dragover', ['$event'])
   onDragOver(event: DragEvent) {
     event.preventDefault();
+    const afterElement = this.getDraggableAfterElement(event.clientY);
     const draggable = document.querySelector('.dragging');
-    this.elementRef.nativeElement.appendChild(draggable);
+    
+    if (afterElement == null) {
+      this.elementRef.nativeElement.appendChild(draggable);
+    } else {
+      this.elementRef.nativeElement.insertBefore(draggable, afterElement);
+    }
   }
 
-  constructor(private elementRef: ElementRef) { }
+  private getDraggableAfterElement(y: number) {
+    const draggableElements = [...this.elementRef.nativeElement.querySelectorAll('.draggable:not(.dragging)')];
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+  }
 
 }
--- projects/angular-dnd/src/lib/draggable.directive.ts
+++ projects/angular-dnd/src/lib/draggable.directive.ts
@@ -1,23 +1,35 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[libDraggable]'
+  selector: '[libDraggable]',
+
 })
 export class DraggableDirective {
 
+  constructor(public elementRef: ElementRef) {
+    this.elementRef.nativeElement.style.cursor = 'move';
+    this.elementRef.nativeElement.draggable = true;
+    this.elementRef.nativeElement.classList.add('draggable');
+  }
+
   @HostListener('dragstart', ['$event'])
-  onDragStart(_: DragEvent) {
+  onDragStart(event: DragEvent) {
+    // TODO: Clone node, add to DOM and position in the drag event
+    // event.dataTransfer.setDragImage(this.elementRef.nativeElement, 0, 0);
     this.elementRef.nativeElement.classList.add('dragging');
+    this.elementRef.nativeElement.style.opacity = 0.3;
   }
 
   @HostListener('dragend', ['$event'])
   onDragEnd(_: DragEvent) {
     this.elementRef.nativeElement.classList.remove('dragging');
+    this.elementRef.nativeElement.style.opacity = 1;
   }
 
-  constructor(private elementRef: ElementRef) {
-    this.elementRef.nativeElement.style.cursor = 'move';
-    this.elementRef.nativeElement.draggable = true;
+  @HostListener('drag', ['$event'])
+  onDrag(event: DragEvent) {
+    // TODO: Position ghost clone element
+    // console.debug(event);
   }
 
 }
